40b08cdfda0c700344fa0e95b01bb3aadcba41ee
YevKrasnenkov <krasnenkov.evgen@gmail.com>
2025-07-24 12:24:46 -0400

    Add SoutHelloJava class to demonstrate string manipulation and indexing

---
+++ tasks/src/main/java/org/kras/SoutHelloJava.java
@@ -0,0 +1,16 @@
+package org.kras;
+
+public class SoutHelloJava {
+    public static void main(String[] args) {
+        String a = "Hello! Java";
+        int startIndexOfJava = a.indexOf("Java");// 7
+        System.out.println(startIndexOfJava);
+        String replace = a.replace("Hello!", "Welcome!");
+        int newIndex = a.indexOf("Java");//9
+        System.out.println(newIndex);
+        StringBuilder b = new StringBuilder(a);
+        int i = b.indexOf("Java");//9
+        System.out.println(i);
+
+    }
+}
